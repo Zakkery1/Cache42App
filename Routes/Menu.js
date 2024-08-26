028066c623d9505556b86884e1be4fa1c8be3afe
Zakkery Cales <zakkerycales@gmail.com>
2024-08-25 20:34:31 -0500

    Menu Update

--- Routes/Menu.js
+++ Routes/Menu.js
@@ -1,57 +1,61 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, ImageBackground, Image } from "react-native";
-import * as React from "react";
-import { BottomNavigation, Text } from "react-native-paper";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import MenuList from "./MenuList";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, View, ImageBackground, Image } from "react-native";
+// import * as React from "react";
+// import { BottomNavigation, Text } from "react-native-paper";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+// import MenuList from "./MenuList";
 
+// export default function MenuRoute() {
+//   const image = "";
+//   return (
+//     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+//       <View style={styles.container}>
+//         <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+//         <Image
+//           style={styles.tinyLogo}
+//           source={{
+//             uri: "https://static.wixstatic.com/media/de49ba_6b0e5f6b8af64dac9b81d7f809833d98~mv2.png/v1/fill/w_210,h_174,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/de49ba_6b0e5f6b8af64dac9b81d7f809833d98~mv2.png",
+//           }}
+//         />
+//         <Text style={styles.text}>Menu</Text>
+//       </View>
+//       <View>
+//         <MenuList />
+//       </View>
+//     </ImageBackground>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: "column",
+//   },
+//   image: {
+//     zIndex: 0,
+//     flex: 1,
+//     resizeMode: "cover",
+//     justifyContent: "center",
+//   },
+//   bottomNavigation: {
+//     zIndex: 1,
+//   },
+//   tinyLogo: {
+//     width: 50,
+//     height: 50,
+//     alignSelf: "center",
+//     top: 80,
+//   },
+//   text: {
+//     color: "white",
+//     fontSize: 35,
+//     top: 85,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//     marginTop: 20,
+//   },
+// });
+import AccordionView from "./test";
 export default function MenuRoute() {
-  const image = "";
-  return (
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <View style={styles.container}>
-        <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
-        <Image
-          style={styles.tinyLogo}
-          source={{
-            uri: "https://static.wixstatic.com/media/de49ba_6b0e5f6b8af64dac9b81d7f809833d98~mv2.png/v1/fill/w_210,h_174,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/de49ba_6b0e5f6b8af64dac9b81d7f809833d98~mv2.png",
-          }}
-        />
-        <Text style={styles.text}>Menu</Text>
-      </View>
-      <View>
-        <MenuList />
-      </View>
-    </ImageBackground>
-  );
+  return <AccordionView />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  image: {
-    zIndex: 0,
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-  },
-  bottomNavigation: {
-    zIndex: 1,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-    alignSelf: "center",
-    top: 80,
-  },
-  text: {
-    color: "white",
-    fontSize: 35,
-    top: 85,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginTop: 20,
-  },
-});
---
+++ Routes/test.js
@@ -0,0 +1,210 @@
+import React, { Component } from "react";
+import {
+  Switch,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import Constants from "expo-constants";
+import Accordion from "react-native-collapsible/Accordion";
+
+const windowWidth = Dimensions.get("window").width;
+const SECTIONS = [
+  {
+    title: "Small Plates",
+    content: `Lobster Queso with warm chips - $20\n
+              Char Grilled Oysters - $22\n
+              Smoked Wings (Bourbon Glazed, Cajun Dusted, Buffalo) - $18\n
+              Bourbon Lollipop Lamb - $35\n
+              OxTail Egg Rolls - $22\n
+              Lobster Menage (3 Cold Water Tails 3 Ways - Grilled, Fried, and Stuffed, served with 3 sauces) - $51\n
+              Jumbo Fried Shrimp (Served with double dipping sauces) - $22\n
+              Lobster Queso Fries (House-made fries with a jalapeño queso and warm butter-poached lobster) - $21\n
+              Jumbo Shrimp Cocktail (Jumbo prawns served with cocktail and spicy remoulade) - $20\n
+              Buffalo Egg Rolls - $20`,
+    image: require("../assets/Cache42foodpic1.jpg"),
+  },
+  {
+    title: "Salads",
+    content: `
+      42 Chopped Salad - $14\n
+      42 Caesar Salad - $14\n
+      42 Wedge Ice Salad - $14\n
+      42 House Salad - $14`,
+    image: require("../assets/Cache42foodpic2.jpg"),
+  },
+  {
+    title: "Off The Grill",
+    content: `
+      40oz Prime Tomahawk - $135\n
+      Golden Rack Of Lamb - Market Price\n
+      Surf And Turf (8oz Filet Mignon with a delicious grilled lobster tail served with melted garlic butter and lemon) - $68\n
+      Cajun NY Strip with Shrimp - $55`,
+    image: require("../assets/Cache42foodpic3.jpg"),
+  },
+  {
+    title: "Mains",
+    content: `
+      Jamaican Oxtails Over Rice - $38\n
+      King Salmon And Shrimp - $42\n
+      Cache Cajun Pasta (Options: Shrimp, Chicken, Lamb, Lobster, Salmon) - $28`,
+    image: require("../assets/Cache42foodpic4.jpg"),
+  },
+  {
+    title: "Side Items",
+    content: `
+      Lobster Mashed Potatoes - $23\n
+      1LB Baked Potatoes - $14\n
+      Garlic Mashed Potatoes - $13\n
+      Brussel Sprouts - $13\n
+      Queso Blanco Lobster Mac - $23\n
+      Creamed Spinach - $12\n
+      Sautéed Asparagus - $13`,
+    image: require("../assets/Cache42foodpic5.jpg"),
+  },
+  {
+    title: "Cocktails",
+    content: `
+      House Martini - $12\n
+      Top Shelf Martini - $15\n
+      Bubbly Martini - $15\n
+      Cotton Candy Margarita/Martini - $15\n
+      Wokesha - $20\n
+      Strawberry Casamigos - $14\n
+      Strawberry Hennessy - $14\n
+      Old Fashion - $12\n
+      Long Island - $10\n
+      Walk Me Down - $10\n
+      Amaretto Sour - $10\n
+      Top Shelf Margarita - $17\n
+      House Margarita - $12\n
+      Mai Tai - $10\n
+      Screwdriver - $10\n
+      Tequila Sunrise - $10\n
+      Green Tea Shot - $10\n
+      Don Julio 1942 - $35\n
+      Don Julio Rosado - $30`,
+    image: require("../assets/Cache42foodpic6.jpg"),
+  },
+];
+
+export default class AccordionView extends Component {
+  state = {
+    activeSections: [],
+  };
+
+  _renderSectionTitle = (section) => {
+    return <View style={styles.content}></View>;
+  };
+
+  _renderHeader = (section) => {
+    return (
+      <View style={styles.header}>
+        {/* <Text style={styles.headerText}>{section.title}</Text> */}
+        <Text style={styles.headerText}>{section.title}</Text>
+        <ImageBackground
+          style={{ height: 70, maxWidth: windowWidth }}
+          source={section.image}
+        ></ImageBackground>
+      </View>
+    );
+  };
+
+  _renderContent = (section) => {
+    return (
+      <View style={styles.content}>
+        <Text>{section.content}</Text>
+      </View>
+    );
+  };
+
+  _updateSections = (activeSections) => {
+    this.setState({ activeSections });
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <Accordion
+          sections={SECTIONS}
+          activeSections={this.state.activeSections}
+          renderSectionTitle={this._renderSectionTitle}
+          renderHeader={this._renderHeader}
+          renderContent={this._renderContent}
+          onChange={this._updateSections}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF",
+    paddingTop: Constants.statusBarHeight,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 22,
+    fontWeight: "300",
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: "#F5FCFF",
+    padding: 10,
+    // borderWidth: 3,
+    top: 20,
+  },
+  headerText: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  content: {
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  active: {
+    backgroundColor: "rgba(255,255,255,1)",
+  },
+  inactive: {
+    backgroundColor: "rgba(245,252,255,1)",
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  selector: {
+    backgroundColor: "#F5FCFF",
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: "bold",
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: "500",
+    padding: 10,
+  },
+  multipleToggle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 30,
+    alignItems: "center",
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+  image: {
+    height: 35,
+    width: 50,
+  },
+});
